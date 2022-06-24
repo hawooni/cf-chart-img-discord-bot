77ac1d31ac7958cba6e25853064512b95a773c6c
hawooni <hawooni@protonmail.com>
2022-06-24 18:46:38 +0000

    update discord webhook payload signature validation with lib discord-interactions

--- src/controller/discord.js
+++ src/controller/discord.js
@@ -1,7 +1,7 @@
+import { InteractionResponseType, InteractionType, verifyKey } from 'discord-interactions'
 import { JsonResponse } from '../helper/response'
-import { InteractionType, InteractionResponseType } from '../helper/interaction'
-import { message as MESSAGE } from '../../config'
 import { appCommand } from '../service/discord'
+import { message as MESSAGE } from '../../config'
 
 export const webhook = (req, env) => {
   const { DEBUG } = env
@@ -12,16 +12,17 @@ export const webhook = (req, env) => {
       const { type } = payload
 
       if (type === InteractionType.PING) {
-        DEBUG && console.log(':: debug :: discord :: payload type :: PING')
+        DEBUG && console.log(':: debug :: discord :: webhook payload type :: PING')
         return new JsonResponse({ type: InteractionResponseType.PONG })
       }
 
       if (type === InteractionType.APPLICATION_COMMAND) {
-        DEBUG && console.log(':: debug :: discord :: payload type :: APPLICATION_COMMAND')
+        DEBUG && console.log(':: debug :: discord :: webhook payload type :: APPLICATION_COMMAND')
         return appCommand(payload, env)
       }
 
-      return new JsonResponse({ error: `Payload type is not supported.` }, { status: 400 })
+      DEBUG && console.log(`:: debug :: discord :: webhook payload type (${type}) is not supported`)
+      return new JsonResponse({ error: 'The payload type is not supported.' }, { status: 400 })
     })
     .catch((error) => {
       if (DEBUG) {
@@ -44,9 +45,14 @@ export const verifySignature = async (req, env) => {
       const timestamp = req.headers.get('x-signature-timestamp')
       const body = await req.clone().arrayBuffer()
 
-      // todo: verify signature
+      if (verifyKey(body, signature, timestamp, DISCORD_PUBLIC_KEY)) {
+        console.log(':: debug :: discord :: webhook payload signature verifyKey success')
+      } else {
+        console.log(':: error :: discord :: webhook payload signature verifyKey failed')
+        return new JsonResponse({ error: 'The server signature verification failed.' }, { status: 401 })
+      }
     } else {
-      console.log(':: error :: environment variable DISCORD_PUBLIC_KEY is required')
+      console.log(':: error :: discrod :: DISCORD_PUBLIC_KEY is required')
       return new JsonResponse({ error: 'The server has encountered a configuration error.' }, { status: 500 })
     }
   }
